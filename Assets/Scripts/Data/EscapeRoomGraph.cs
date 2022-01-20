bae77b1decca8d649fde2e151a91e63d8e7c8ec5
LapisRaider <angielimenqi7@gmail.com>
2022-01-20 22:02:30 +0800

    use another node template

--- Assets/Scripts/Data/EscapeRoomGraph.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-namespace XNode.Examples {
-    /// <summary> Defines an example nodegraph that can be created as an asset in the Project window. </summary>
-    [Serializable, CreateAssetMenu(fileName = "New Graph", menuName = "NUSGDG/Graph")]
-    public class EscapeRoomGraph : NodeGraph { }
-}
