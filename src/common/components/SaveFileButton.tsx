c9482db41268e6727bc3f5151d19ffd7cef6a608
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-01-29 18:31:35 +0800

    feat: Write state to json file
    Includes pretty printing

--- src/common/components/SaveFileButton.tsx
+++ src/common/components/SaveFileButton.tsx
@@ -1,5 +1,6 @@
 import React, {ReactNode, useRef} from 'react'
 import {Button} from '@chakra-ui/react'
+import {useRoot} from "../../hooks/useRoot";
 
 interface SaveFileButtonProps {
     type: string,
@@ -8,13 +9,18 @@ interface SaveFileButtonProps {
 }
 
 export const SaveFileButton = ({type, filename = "game.json", children}: SaveFileButtonProps) => {
+    const ctx = useRoot();
     const fileOutput = useRef<HTMLAnchorElement | null>(null);
+
+    const replacer = null;
+    const spacer = "    "; // 4 spaces
 
     const handleClick = () => {
         if (!fileOutput.current) {
             return;
         }
-        const file = new Blob(["{\"potato\": 100}"], {type: type});
+        const contents = JSON.stringify(ctx.state, replacer, spacer);
+        const file = new Blob([contents], {type: type});
         fileOutput.current.href = URL.createObjectURL(file);
         fileOutput.current.download = filename;
         fileOutput.current?.click();
