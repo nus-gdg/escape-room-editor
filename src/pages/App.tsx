a15c30a4ec55095f348fdca578635fcbed09c985
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-03-05 20:48:09 +0800

    feat: Add File Toolbar in General Page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,11 +3,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import {RootProvider} from "../common/containers/Root";
 import {GeneralPage} from "./GeneralPage/GeneralPage";
 import {ContentPage} from "./ContentPage/ContentPage";
+import {Toolbar} from "./Toolbar/Toolbar";
 
 export const App = () => (
     <ChakraProvider resetCSS>
         <RootProvider>
-            <ContentPage />
+            <Toolbar />
+            <GeneralPage />
         </RootProvider>
     </ChakraProvider>
 )
