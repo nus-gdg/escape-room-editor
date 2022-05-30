c3fe1036764eb2e171579751cb9e49a0f3bd3aaf
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-05-31 03:22:30 +0800

    feat: Create ContentEditor

---
+++ src/pages/GeneralPage/components/ContentEditor.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { ContentData } from "../Data/RoomData";
+import { ContentObject } from "../../../common/components/ContentObject";
+import { useRoot } from "../../../hooks/useRoot";
+import "./ContentEditor.css"
+
+interface ContentEditorProps {
+    content: ContentData;
+    onUpdateContent: (
+        updatedContent: ContentData,
+        varName: keyof ContentData
+    ) => void;
+}
+
+export const ContentEditor = (props: ContentEditorProps) => {
+    const ctx = useRoot();
+
+    //update the content of the currRoom
+    function onChangeContentData(newData: string, varName: keyof ContentData) {
+        let updateContent = { ...props.content };
+        updateContent[varName] = newData as never;
+
+        props.onUpdateContent(updateContent, varName);
+    }
+
+    return (
+        <div className={"content-editor"} >
+            <div className={"content-editor-label"} ># Room Editor</div>
+            <div className={"content-editor-body"}>
+                <div className={"content-editor-form"}>
+                    <input className={"content-editor-title"}
+                           value={props.content.title}
+                           placeholder="Name"
+                           onChange={(event) => onChangeContentData(event.currentTarget.value, "title")}
+                    />
+                    <textarea className={"content-editor-description"}
+                              value={props.content.description}
+                              placeholder="Description"
+                              onChange={(event) => onChangeContentData(event.currentTarget.value, "description")}
+                    />
+                    <input className={"content-editor-image-url"}
+                           value={props.content.imageLink}
+                           placeholder="Image url"
+                           onChange={(event) => onChangeContentData(event.currentTarget.value, "imageLink")}
+                    />
+                </div>
+                <ContentObject title={props.content.title}
+                               description={props.content.description}
+                               imageUrl={props.content.imageLink} />
+            </div>
+        </div>
+    )
+};
