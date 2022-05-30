9f47d60e3a4e67121c0132a15cf83e14ef7952ac
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-05-30 22:16:34 +0800

    feat: Add markdown and emojis in ContentObject

--- src/common/components/ContentObject.tsx
+++ src/common/components/ContentObject.tsx
@@ -1,11 +1,9 @@
-import React, {CSSProperties, useRef} from 'react'
-import {
-    Box,
-    Image,
-    Text,
-    Heading,
-} from '@chakra-ui/react'
-import {Emoji} from "emoji-mart";
+import React from 'react'
+import {NimbleEmoji} from "emoji-mart";
+import emojiDatas from 'emoji-mart/data/twitter.json';
+import {MarkdownRules, rules, toHTML} from "discord-markdown";
+import SimpleMarkdown from 'simple-markdown';
+import "./ContentObject.css"
 
 interface ContentProps {
     title: string
@@ -33,109 +31,46 @@ export const ContentObject = ({title = "Title",
         return text ? <div key={index}>{text}</div> : <br/>;
     }
 
-    const style: CSSProperties = {
-        display: "flex",
-        borderRadius: "5px",
-        backgroundColor: "#aaaaaa",
-    }
+    const customRules: MarkdownRules = Object.assign(rules, {
+            discordEmoji: {
+                order: SimpleMarkdown.defaultRules.strong.order,
+                match: (source: string) => /^(:\w+:)/.exec(source),
+                parse: (capture: SimpleMarkdown.Capture) => {
+                    return {
+                        emoji: capture[1],
+                    };
+                },
+                html: (node: SimpleMarkdown.SingleASTNode, output: SimpleMarkdown.Output<string>, state: SimpleMarkdown.State) => {
+                    const emoji: string = node.emoji;
+                    return String(
+                        NimbleEmoji({
+                            html: true,
+                            set: `twitter`,
+                            data: emojiDatas,
+                            emoji: emoji,
+                            size: 21,
+                            children: emoji,
+                        })
+                    )
+                }
+            }
+        });
 
-    const lineStyle: CSSProperties = {
-        width: "5px",
-        borderTopLeftRadius: "5px",
-        borderBottomLeftRadius: "5px",
-        backgroundColor: "#bd7ef9",
-    }
-
-    const bodyStyle: CSSProperties = {
-        height: "100%",
-        maxWidth: "calc(70% - 10px)",
-        paddingTop: "8px",
-        paddingBottom: "10px",
-        paddingLeft: "10px",
-        paddingRight: "10px",
-        borderTopRightRadius: "5px",
-        borderBottomRightRadius: "5px",
-        backgroundColor: "#2F3136",
-    }
-
-    const titleStyle: CSSProperties = {
-        fontSize: "medium",
-        fontFamily: "sans-serif",
-        color: "#FFFFFF",
-    }
-
-    const descriptionStyle: CSSProperties = {
-        marginTop: "10px",
-        fontSize: "small",
-        fontFamily: "sans-serif",
-        color: "#FFFFFF",
-    }
-
-    const imageStyle: CSSProperties = {
-        maxHeight: "300px",
-        marginTop: "15px",
-        borderRadius: "5px",
-        objectFit: "scale-down",
-    }
-
-    const ref = useRef(null);
-
-    const emojiRegex = new RegExp(`(:[a-zA-Z0-9-_+]+:)`, 'g');
-    const emojiSize = 24;
-
-    function createEmoji(emoji: string) {
-        return (
-            <>
-                {emoji && (
-                    <Emoji size={emojiSize}
-                       emoji={emoji}
-                       fallback={() => emoji as any} />
-                )}
-            </>
-        );
-    }
-
-    function emojifyString(value: string) {
-        return (
-            <>
-                {value.split(emojiRegex).map(createEmoji)}
-            </>
-        );
-    }
-
-    const italicsRegex = new RegExp(`\\*(.+?)\\*`, 'g');
-    const boldRegex = new RegExp(`\\*\\*(.+?)\\*\\*`, 'g');
-
-    function createBoldText(text: string) {
-        return (
-            <>
-                {text && (
-                    <Text as='b'>
-                        {text}
-                    </Text>
-                )}
-            </>
-        );
-    }
-
-    //8e2dd6c23ccf587435feaf7ff463c792
+    // @ts-ignore
+    const parser = SimpleMarkdown.parserFor(customRules);
+    const htmlOutput = SimpleMarkdown.outputFor(customRules, 'html')
 
     return (
-        <Box style={style}>
-            <Box style={lineStyle} />
-            <Box style={bodyStyle}>
-                <Heading as='h1' style={titleStyle} >
-                    {title}
-                </Heading>
-                <Text style={descriptionStyle} >
-                    {emojifyString(description)}
-                    <br/>
-                    {createBoldText(description)}
-                    <br/>
-                    {"****potato****".split(boldRegex)}
-                </Text>
-                <Image style={imageStyle} src={imageUrl} />
-            </Box>
-        </Box>
-    );
+        <div className={"content"}>
+            <div className={"content-line"} />
+            <div className={"content-body"}>
+                <h1 className={"content-title"} >
+                    <strong children={title} />
+                </h1>
+                <div className={"content-description"}
+                     dangerouslySetInnerHTML={{__html: toHTML(description, {}, parser, htmlOutput)}} />
+                <img className={"content-image"} src={imageUrl} alt={"Missing Image"}/>
+            </div>
+        </div>
+    )
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import { extendTheme, ChakraProvider, Box } from '@chakra-ui/react'
+import React, {CSSProperties} from 'react'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 import {RootProvider} from "../common/containers/Root";
 import {ContentObject} from "../common/components/ContentObject";
 
 export const App = () => {
+    const title = "Carrots GET!"
+
     const hamsterImg = "https://i.pinimg.com/originals/40/87/a6/4087a632b7855c85c66294e76e1ed81d.jpg";
     const birdImg = "https://i.pinimg.com/736x/5e/2b/74/5e2b74b9ba6e196d5837ef586eb70794.jpg";
+    const missingImg = "https://ir.pinimg.com/736x/5e/2b/74/5e2b74b9ba6e196d5837ef586eb70794.jpg";
+
+    const d1 = "Potatoes :potato: <b>hello</b> ***are*** **very** *delicious* ~~right?~~ `Agreed!` __yes__ _i_ do <#30333>";
+    const d2 = "hello <b>hello</b> <:carrot:881406221654044682> hello";
+    const d3 = "why is this ```working```";
+    const d4 = "yummy :carrot: :third_place_medal: :woman-swimming: carrots";
+    const d5 = "yummy";
+
+    const paddingStyle: CSSProperties = {
+        width: "100%",
+        height: "100%",
+        padding: "30px",
+    }
 
     return (
         <div style={{
@@ -15,11 +30,11 @@ export const App = () => {
             <ChakraProvider resetCSS>
                 <RootProvider>
                     {/*<Toolbar />*/}
-                    <Box width={"100%"} height={"100%"} padding={"30px"}>
-                        <ContentObject title={"carrot"}
-                                       description={"Potatoes :potato: are *delicious*"}
-                                       imageUrl={birdImg} />
-                    </Box>
+                    <div style={paddingStyle} >
+                        <ContentObject title={title}
+                                       description={d1}
+                                       imageUrl={hamsterImg} />
+                    </div>
                     {/*<EmojiMenu />*/}
                     {/*<GeneralPage />*/}
                     {/*<ContentPage />*/}
