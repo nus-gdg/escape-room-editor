2f2bda10b2dd4d0155e21adee126e9c83f411b79
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-06-11 23:42:40 +0800

    feat: Fix uncaught exception in useRoot hook

--- src/hooks/useRoot.tsx
+++ src/hooks/useRoot.tsx
@@ -1,9 +1,10 @@
+import { isEmpty } from "lodash";
 import React, {useContext} from "react";
 import {Root} from "../common/containers/Root";
 
 export const useRoot = () => {
     const ctx = useContext(Root);
-    if (ctx === undefined) {
+    if (isEmpty(ctx)) {
         throw new Error("useRoot must be used within RootStoreProvider")
     }
     return ctx;
