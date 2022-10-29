620b5ad711a35b197fe6e13a497c257f257e55d4
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-10-29 14:37:15 +0800

    state

--- src/containers/menus/RoomsMenu.tsx
+++ src/containers/menus/RoomsMenu.tsx
@@ -2,19 +2,7 @@ import {memo} from "react";
 import {useDispatch, useSelector} from "../../app";
 import NavMenu from "../../components/navigation/NavMenu";
 import {createRoom, deleteRooms, openFlow, selectRooms, updateRoom} from "../../slices";
-
-function createDefaultName(names: string[], prefix = "") {
-    const regex = new RegExp(prefix);
-    const counts: number[] = [];
-    for (const name of names) {
-        const count = parseInt(name.replace(regex, ""));
-        if (!isNaN(count)) {
-            counts.push(count);
-        }
-    }
-    const maxCount = Math.max(0, ...counts);
-    return `${prefix}${maxCount + 1}`;
-}
+import {createDefaultName} from "./utils";
 
 const RoomsMenu = () => {
     const dispatch = useDispatch();
