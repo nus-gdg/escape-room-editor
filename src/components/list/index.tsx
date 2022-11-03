878e78be175132e082518b5bc7f9f912319abc87
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2022-11-03 09:04:59 +0800

    forms

--- src/components/list/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from "../forms/List";
-export * from "../forms/ListHeader";
-export * from "../forms/ListItem";
-
-export {default as List} from "../forms/List";
-export {default as ListHeader} from "../forms/ListHeader";
-export {default as ListItem} from "../forms/ListItem";
